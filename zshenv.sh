13ed8b97ef70926e7caacd3ece6698dc5705bde1
Walter Oggioni <oggioni.walter@gmail.com>
2024-05-07 08:04:21 +0800

    added cargo bin directory to PATH

--- zshenv.sh
+++ zshenv.sh
@@ -5,6 +5,7 @@ export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
 export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
 
+#export GRAALVM_HOME="/usr/lib/jvm/java-17-graalvm"
 #export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dsun.java2d.opengl=true'
 export EDITOR=vim
-export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.nimble/bin:$PATH
+export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.nimble/bin:$PATH:$HOME/.cargo/bin
